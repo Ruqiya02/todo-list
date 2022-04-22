bde86ce762d5e2c7f9e493085b834deaab464fb3
Heyderi Ruqiyye <heyderliruqiyye2@gmail.com>
2022-04-22 17:11:40 +0400

    start

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
-const input=document.querySelector('input');
-const list=document.querySelector('ul')
-const button=document.querySelector('.buttons')
-button.addEventListener('keyup',(event)=>{
-    event.preventDefault();
-    if(event.keyCode===13 && input.value!=='' && input.value!==' '){
-        let listElement=document.createElement('li')
-        listElement.innerText=input.value
-        list.appendChild(listElement)
-        console.log(listElement)
+const todoİnput=document.querySelector('input');
+const todoList=document.querySelector('.todo-list');
+const AddBtn=document.querySelector('.button2');
+const delButton=document.querySelector('.del');
+document.addEventListener('keyup',(event)=>{
+    if(event.keyCode==13){
+        if(todoİnput.value!==""&&todoİnput.value!==" "){
+            event.preventDefault();
+            let listElement=document.createElement('div')
+            listElement.classList.add('.input-container')
+            listElement.innerHTML=todoİnput.value
+
+            todoList.append(listElement)
+        }
     }
+})
+delButton.addEventListener('click',()=>{
+    todoİnput.value=''
 })
 
